474c7cc819721a341c8d8be0b879ef20a6dcdebe
alireza <bagherialireza.com@gmail.com>
2023-08-09 20:55:52 +0330

    project1react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const tick= ()=>{
+    const elem = (
+        <div>
+            <h1>
+                alireza
+            </h1> 
+            <h1>
+                time is {new Date().toLocaleTimeString()}
+            </h1>
+        </div>
+    )
+    ReactDOM.render(elem,document.getElementById('root'));
+};
+setInterval(()=>{
+    tick();
+},1000);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
