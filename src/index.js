106f2b95f00a8997c4ada02259e54e41e1c45df8
alireza <bagherialireza.com@gmail.com>
2023-08-09 21:33:41 +0330

    #component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const tick= ()=>{
-    const elem = (
-        <div>
-            <h1>
+import {createRoot} from 'react-dom/client';
+
+// const tick= ()=>{
+//     const elem = (
+//         <div>
+//             <h1>
+//                 alireza
+//             </h1> 
+//             <h1>
+//                 time is {new Date().toLocaleTimeString()}
+//             </h1>
+//         </div>
+//     )
+//     ReactDOM.render(elem,document.getElementById('root'));
+// };
+// setInterval(()=>{
+//     tick();
+// },1000);
+
+
+class App extends React.Component{
+    render(){
+        return (
+                     <div>
+           <h1>
                 alireza
             </h1> 
-            <h1>
-                time is {new Date().toLocaleTimeString()}
+             <h1>
+                 time is {new Date().toLocaleTimeString()}
             </h1>
-        </div>
-    )
-    ReactDOM.render(elem,document.getElementById('root'));
+         </div>
+        )
+    }
+};
+
+
+
+const tick=()=>{
+    // ReactDOM.render(<App/>,document.getElementById('root'))
+
+    createRoot(document.getElementById('root')).render(<App/>)
+
 };
 setInterval(()=>{
-    tick();
+    tick()
 },1000);
-
-
-
-
-
-
-
 
 
 
