141be2a366fd9f0c5fbd5889259fa89450e821db
alireza <bagherialireza.com@gmail.com>
2023-08-10 09:47:58 +0330

    #8

--- src/index.js
+++ src/index.js
@@ -1,52 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
 import {createRoot} from 'react-dom/client';
 
-// const tick= ()=>{
-//     const elem = (
-//         <div>
-//             <h1>
-//                 alireza
-//             </h1> 
-//             <h1>
-//                 time is {new Date().toLocaleTimeString()}
-//             </h1>
-//         </div>
-//     )
-//     ReactDOM.render(elem,document.getElementById('root'));
-// };
-// setInterval(()=>{
-//     tick();
-// },1000);
+import './style.css'
 
 
-class App extends React.Component{
+// class Timer extends React.Component{
+//     constructor(){
+//         super();
+//         this.state = {
+//             time : new Date().toLocaleTimeString()
+//         };
+//     };
+//     render(){
+//         setInterval(()=>{
+//             this.setState({
+//                 time : new Date().toLocaleTimeString()
+//             })
+//         },1000)
+//         return(
+//             <div className='main'>
+//             <h2 className='time'>
+//             {this.state.time}
+//             </h2>
+//             </div>
+//         )
+        
+//     }
+// }
+class Timer extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            time:new Date().toLocaleTimeString()
+        }
+    }
     render(){
-        return (
-                     <div>
-           <h1>
-                alireza
-            </h1> 
-             <h1>
-                 time is {new Date().toLocaleTimeString()}
-            </h1>
-         </div>
+        setInterval(() => {
+            this.setState({
+                time:new Date().toLocaleTimeString()
+            })
+        }, 1000);
+        return(
+            <div className='main'>
+                <h1 className='time'>
+                    {this.state.time}
+                </h1>
+            </div>
         )
     }
-};
-
-
-
-const tick=()=>{
+}
     // ReactDOM.render(<App/>,document.getElementById('root'))
-
-    createRoot(document.getElementById('root')).render(<App/>)
-
-};
-setInterval(()=>{
-    tick()
-},1000);
-
+    createRoot(document.getElementById('root')).render(<Timer/>)
 
 
