e8b515ebc6a65fefdf1379703a36746dbc1229c7
alireza <bagherialireza.com@gmail.com>
2023-08-12 08:01:34 +0330

    10

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var interval=null;
 
 class Timer extends React.Component{
     constructor(){
@@ -9,18 +10,20 @@ class Timer extends React.Component{
             time:new Date().toLocaleTimeString()
         }
     }
-    render(){
+    componentDidMount(){
         setInterval(() => {
             this.setState({
                 time:new Date().toLocaleTimeString()
             })
         }, 1000);
+        
+    }
+
+    render(){
         return(
-            
             <h1 className='time'>
                 {this.state.time}
-            </h1>
-            
+            </h1>  
         )
     }
 };
